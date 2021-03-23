d99cf0d87242951deee74d97932e83ba050385e5
bniewelt <79923446+TheSmaszer@users.noreply.github.com>
2021-03-23 16:42:35 +0100

    Wprowadzono poprawki

--- plotsin.py
+++ plotsin.py
@@ -11,16 +11,22 @@ def frange(a,b,step):
         a = a + step
     return lst
 # print(frange(1,5,0.2)
-def plotf(func, a, b):
+def plotf(func, a, b,title=""):
     x = frange(a,b,0.01)
     plt.plot(x,func(x))
+    plt.xlabel("Wartości $x$")
+    plt.ylabel("Wartości funkcji")
+    plt.title(title)
+    plt.grid()
     plt.show()
 
 def plotsin(a,b):
     def f(k):
         return np.multiply(k,np.sin(k)) - np.multiply(k,k)
-    plotf(f,a,b)
+    plotf(f,a,b,title = "Wykres $f \, (x) = x \: \sin x - x^2$")
 
-a = input("Podaj lewy przedział: ")
-b = input("Podaj prawy przedział: ")
-plotsin(a,b)
+# a = input("Podaj lewy przedział: ")
+# b = input("Podaj prawy przedział: ")
+# plotsin(a,b)
+
+plotsin(-10,10)
