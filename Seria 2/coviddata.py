d5faa119e4441fb97401ccabbd4b57c21c05b0b1
bniewelt <79923446+TheSmaszer@users.noreply.github.com>
2021-04-01 22:48:28 +0200

    Poprawiono coviddata.py

--- Seria 2/coviddata.py
+++ Seria 2/coviddata.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def update():
     import requests
     r = requests.get("https://covid.ourworldindata.org/data/jhu/full_data.csv")
@@ -5,16 +8,39 @@ def update():
     f.write(r.content.decode("utf-8"))
     f.close()
 
-# update()
-
-# print(lista[0].split(sep=","))
-# print(lista[50].split(sep=","))
-
+update()
+avail_count = {}
 with open("full_data.csv") as lista:
+    next(lista)
     for line in lista:
         line_sep = line.split(sep=",")
-        case_pol = {}
-        if line_sep[1] == "Poland":
-            case_pol = {case_pol,line_sep[0]:line_sep[2]}
+        avail_count[line_sep[1]] = ""
 
-print(case_pol)
+countries = ""
+for i in avail_count.keys():
+    countries += i + ", "
+
+cases = {}
+
+while(True):
+    print("Available countires: ",countries[:-2])
+    country = input("Enter country: ")
+    if(not cases.get(country)):
+        case_count = {}
+        with open("full_data.csv") as lista:
+            for line in lista:
+                line_sep = line.split(sep=",")
+                if line_sep[1] == country:
+                    case_count[line_sep[0]] = line_sep[2]
+
+        if(case_count):
+            cases[country] = case_count
+        else:
+            print("404 - Country not found")
+            continue
+    plt.figure(figsize=(12, 8))
+    plt.bar(cases[country].keys(),[float(value) for value in cases[country].values()],label=f"New cases in {country}")
+    plt.grid()
+    plt.legend()
+    plt.xticks(range(1,len(cases[country].keys()),40))
+    plt.show()
